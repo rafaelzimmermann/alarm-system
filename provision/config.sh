901ff463dac7167cbe967e016db5de5667b14725
Rafael Zimmermann <engzimmermann@gmail.com>
2017-06-10 23:44:33 +0200

    Use systemd instead

--- provision/config.sh
+++ provision/config.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-wget http://node-arm.herokuapp.com/node_latest_armhf.deb -O /tmp/node_latest_armhf.deb
-dpkg -i /tmp/node_latest_armhf.deb
+# wget http://node-arm.herokuapp.com/node_latest_armhf.deb -O /tmp/node_latest_armhf.deb
+# dpkg -i /tmp/node_latest_armhf.deb
+
+cp alarm.service /etc/systemd/system/
