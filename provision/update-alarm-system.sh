d561ecff7181f95dd139a5208bb93fc683b805d1
Rafael Zimmermann <engzimmermann@gmail.com>
2017-06-20 20:05:22 +0200

    Fix update script

--- provision/update-alarm-system.sh
+++ provision/update-alarm-system.sh
@@ -3,5 +3,6 @@
 cd /home/pi/workspace/alarm-system; git pull --dry-run | grep -q -v 'Already up-to-date.' && changed=1
 
 if [ "$changed" -eq "1"]; then
+  cd /home/pi/workspace/alarm-system; git pull
   service alarm restart
 fi
