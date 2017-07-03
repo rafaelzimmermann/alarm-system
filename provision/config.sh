602c866253b92c23b009b90e5d87ee852f2de97a
Rafael Zimmermann <engzimmermann@gmail.com>
2017-07-03 20:37:07 +0200

    Change log file

--- provision/config.sh
+++ provision/config.sh
@@ -3,8 +3,8 @@
 # wget http://node-arm.herokuapp.com/node_latest_armhf.deb -O /tmp/node_latest_armhf.deb
 # dpkg -i /tmp/node_latest_armhf.deb
 
-# cp alarm.service /etc/systemd/system/alarm.service
-# cp alarm-ssh.service /etc/systemd/system/alarm-ssh.service
+cp alarm.service /etc/systemd/system/alarm.service
+cp alarm-ssh.service /etc/systemd/system/alarm-ssh.service
 
-cp update-alarm-system.sh /usr/bin/update-alarm-system
-chmod u+x /usr/bin/update-alarm-system
+# cp update-alarm-system.sh /usr/bin/update-alarm-system
+# chmod u+x /usr/bin/update-alarm-system
