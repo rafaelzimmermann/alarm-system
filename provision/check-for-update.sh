edb5879007f6e96c66eef2ba5fd7c9261285c791
Rafael Zimmermann <engzimmermann@gmail.com>
2020-01-22 21:08:27 +0100

    Fix check-for-update.sh

--- provision/check-for-update.sh
+++ provision/check-for-update.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+cd /home/pi/workspace/alarm-system
+
+git fetch
+
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 
 if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
-    cd /home/pi/workspace/alarm-system; git pull
+else
+    git pull
     service alarm-ssh restart
     service alarm restart
 fi
